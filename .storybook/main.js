ab65e6097b24a1461b3a82451be66cc816e9da05
Albin Groen <albin.groen@gmail.com>
2021-11-09 18:49:50 +0100

    :recycle: Update setup from tsdx to microbundle-ctrl

--- .storybook/main.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  stories: ["../stories/**/*.stories.@(ts|tsx|js|jsx)"],
-  // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
-  typescript: {
-    check: true, // type-check stories during Storybook build
-  },
-};
